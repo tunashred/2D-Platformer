731179aacef045b17e14d8c8d4eb2106685f6d96
tunashred <enachealexandru.traian@gmail.com>
2024-05-13 23:49:17 +0300

    Implemented flying eye waypoint traversal

--- Assets/Scripts/FlyingEye.cs
+++ Assets/Scripts/FlyingEye.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 
 public class FlyingEye : MonoBehaviour
 {
+    public float flightSpeed = 2f;
+    public float waypointReachedDistance = 0.1f;
     public DetectionZone biteDetectionZone;
+    public List<Transform> waypoints;
+
     private Animator animator;
     private Rigidbody2D rb;
-    
+    private Damageable damageable;
+    private Transform nextWaypoint;
+
     public bool hasTarget = false;
+    private int waypointNum = 0;
+
+    public bool CanMove
+    {
+        get { return animator.GetBool(AnimationStrings.canMove); }
+    }
 
     public bool HasTarget
     {
@@ -21,10 +33,53 @@ public class FlyingEye : MonoBehaviour
         }
     }
 
+    private void Flight()
+    {
+        // when you normalize a vector, it's just a direction without any speed
+        Vector2 directionToWaypoint = (nextWaypoint.position - transform.position).normalized;
+
+        // this is the magnitude between the two points
+        float distance = Vector2.Distance(nextWaypoint.position, transform.position);
+
+        rb.velocity = directionToWaypoint * flightSpeed;
+
+        if (distance <= waypointReachedDistance)
+        {
+            waypointNum++;
+            if (waypointNum >= waypoints.Count)
+            {
+                waypointNum = 0;
+            }
+
+            nextWaypoint = waypoints[waypointNum];
+        }
+    }
+
+    private void Start()
+    {
+        nextWaypoint = waypoints[waypointNum];
+    }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        damageable = GetComponent<Damageable>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (damageable.IsAlive)
+        {
+            if (CanMove)
+            {
+                Flight();
+            }
+            else
+            {
+                rb.velocity = Vector3.zero;
+            }
+        }
     }
 
     // Update is called once per frame
